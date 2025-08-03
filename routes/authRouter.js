5db65e441bde49ee0aa767b4dfd1f64b80f57a6a
ShravanRamjathan <shravanramjathan@gmail.com>
2025-08-03 14:34:38 +0200

    refactor: update comments and temporarily disable mail route for future implementation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -14,7 +14,8 @@ router.use(timeLog);
 router.get("/", (req, res) => {
   res.send("Trying to authenticate...");
 });
-router.get("/mail", sendMail);
+// i will add mailing later, focusing on react.
+//router.get("/mail", sendMail);
 // we now added a router to these functions, so when this endpoint is called, that controller logic will run
 router.post("/register", registerUser);
 
