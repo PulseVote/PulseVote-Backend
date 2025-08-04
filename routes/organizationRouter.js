853c7422167964cb55fbce7de5808168b96af40a
ShravanRamjathan <shravanramjathan@gmail.com>
2025-08-04 16:04:56 +0200

    feat: add organization and poll routers with express setup

---
+++ routes/organizationRouter.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+// here we will be defining the creation of an organization
+
+/*
+ post create organization ( owner , time of creation, created id to find , 
+special password to join as a member, email of org, tags
+)
+*/
+
+/* this will be in one filter 
+get org by id
+ 
+get orgs with tag/s
+
+get org by time of creation
+*/
+
+/* accept member into org,
+ */
